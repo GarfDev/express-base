79126a07b2f4e7478c9f983e63b6c8547f7ef914
Nguyen Gia Tuan <tuan.nguyen@hubble.sg>
2020-09-26 22:58:45 +0700

    Initalized Redis

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,40 @@
 import express from "express";
+import mongoose from "mongoose";
+import redis from "redis";
+
+const port = process.env.PORT || 5000;
+
+/*
+ * Initialize Application
+ */
 
 const app = express();
-const port = process.env.PORT || 5000;
+/*
+ * Initialize Redis Connection
+ */
+
+const redisClient = redis.createClient({ url: process.env.REDIS_URL || "" });
+
+redisClient.on("error", (err) => {
+  console.log("Redis error: ", err);
+});
+
+/*
+ * Initialize MongoDB Connection
+ */
+
+mongoose.connect(process.env.MONGODB_URL || "", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+/*
+ * App listening
+ */
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}!`);
